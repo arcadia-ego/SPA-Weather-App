c6b135ac0fea37ff0b9307442ab186ea54165357
arcadia-ego <arcadia-ego@github.com>
2018-08-22 09:38:11 -0700

    initial package setup

---
+++ frontend/src/Root.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducers';
+
+export default ({ children, initialState = {} }) => {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(reduxThunk, logger)
+  );
+
+  return <Provider store={store}>{children}</Provider>;
+};
---
+++ frontend/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from 'react-router-dom';
+//components
+import App from "./App";
+import Root from './Root';
+import Landing from './components/Landing.js';
+//
+
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(
+    <Root>
+        <BrowserRouter>
+            <App >
+            <Route path='/' exact component={Landing} />
+            </App>    
+        </BrowserRouter>
+    </Root>
+, document.getElementById('root'));
+registerServiceWorker()
